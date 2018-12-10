d0eecd9c7ffcb0ad957ce59d147db3bb3dc76fee
Heroel <HeroelJCarpinetti@gmail.com>
2018-12-10 18:08:17 -0300

    Segundo parcial y final de diciembre

---
+++ src/finalDiciembreProgramacionI/main.java
@@ -0,0 +1,114 @@
+package finalDiciembreProgramacionI;
+
+
+/*
+ Realizar un programa que dada una matriz MatSec de caracteres de tamaño MAXFxMAXC con filas
+ compuestas de secuencias de caracteres distintos del caracter espacio (' '), elimine todas las
+ secuencias que tienen orden ascendente entre sus caracteres. Para ello además deberá tener en
+ cuenta que cuando elimina una secuencia debe realizar un corrimiento a izquierda y completar con
+ caracteres espacio (' ') al final de la fila.
+ */
+
+public class main {
+
+	static char[] randomChar = {'A', 'B', 'C', 'D', ' '};
+	
+	//CONSTANTES
+	static final int ROW = 1;
+	static final int COL = 10;
+	
+	public static void loadingMatriz(char[][] matriz) {
+		
+		int randomNumer;
+		
+		for(int i=0; i < ROW; i++) {
+			for(int j=0; j < COL; j++) {
+				
+				if((j == 0) || (j == COL - 1)){
+					matriz[i][j] = ' ';
+				}else {
+				 randomNumer = (int) Math.floor(Math.random() * randomChar.length);
+				 matriz[i][j] = randomChar[randomNumer];
+				}
+			}
+		}
+	}
+	
+	public static void printMatriz(char[][] matriz) {
+		
+		for(int i = 0; i < ROW; i++) {
+			for(int j = 0; j < COL; j++) {
+				System.out.print(matriz[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void searchSequence(char[][] matriz) {
+		
+		boolean initsequence = false;
+		int start = 0;
+		int finish = 0;
+		
+		for(int i = 0; i < ROW; i++) {
+			for(int j = 1; j < COL; j++) {
+				
+				if(matriz[i][j] != ' '){
+					if(!initsequence) {
+						start = j;
+						initsequence = true;
+					}else {
+						finish = j;
+					}
+				}else {
+					
+					if(sequenceAsc(matriz, i, start, finish)) {
+						deleteSequence(matriz, i, start, finish);
+					}
+					
+					//Reseteamos valores
+					initsequence = false;
+					start = 0;
+					finish = 0;
+				}
+			}
+		}
+	}
+	
+	//Verifica si la secuencia esta ordenada ascendentemente
+	public static boolean sequenceAsc(char[][] matriz, int row, int start, int finish) {
+		
+		boolean orderAsc = true;
+
+			for(int i = start; i < finish; i++) {
+				
+				if(matriz[row][i] > matriz[row][i+1]) {
+					orderAsc = false;
+				}
+			}
+			
+			return orderAsc;
+	}
+	
+	//Elimina secuencias
+	public static void deleteSequence(char[][] matriz, int row, int start, int finish) {
+		
+		for(int i = start; i <= finish; i++) {
+			matriz[row][i] = ' ';
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		char[][] matriz = new char[ROW][COL];
+		
+		loadingMatriz(matriz);
+		
+		printMatriz(matriz);
+		
+		searchSequence(matriz);
+		
+		printMatriz(matriz);
+	}
+
+}
---
+++ src/segundoParcialProgramacionI/main.java
@@ -0,0 +1,127 @@
+package segundoParcialProgramacionI;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+ SEGUNDO PARCIAL PROGRAMACIÓN I - 17/05/2018
+Se tiene una matriz MAT de enteros de tamaño 5x20, donde en cada fila se tiene secuencias de
+números distintos de cero separadas por uno o mas ceros. Realizar un programa que para todas
+las filas invierta el contenido de las secuencias que tienen tamaño N, y comienzan y terminan
+con al menos un 1, siendo N un valor entero cargado desde teclado entre 0 y la cantida de
+columnas.
+*/
+
+public class main {
+
+	//CONTANTES
+	final static int COL = 25;
+	final static int FIL = 5;
+	
+	//Cargar valores random en la matriz
+	public static void loadingMatriz(int[][] matriz) {
+		
+		for(int i=0; i < FIL; i++) {
+			for(int j=0; j < COL; j++) {
+				if((j == 0) || (j == COL - 1)) {
+					
+					matriz[i][j] = 0;
+				}else{
+					
+				 matriz[i][j] = (int) Math.floor(Math.random() * 5);	
+				}
+				
+			}
+		}
+		
+	}
+	
+	//Imprimir la matriz
+	public static void printMatriz(int[][] matriz) {
+		for(int i=0; i < FIL; i++) {
+			for(int j=0; j < COL; j++) {
+
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	//Buscar secuencia con X size
+	public static void searchSequence(int[][] matriz, int size) {
+		
+		int counter = 0;
+		int start = 0;
+		int finish = 0;
+
+		for(int i=0; i < FIL; i++){
+			for(int j=0; j < COL; j++) {
+				
+				if(matriz[i][j] != 0) {
+					if(counter == 0) {
+						start = j;
+						counter++;
+					}else {
+						finish = j;
+						counter++;
+					}
+				}else {
+					
+					if(counter == size) {
+						reverseSequense(matriz, i, start, finish);
+					}
+					//Reseteamos todo los contadores
+					start = 0;
+					finish = 0;
+					counter = 0;
+				}
+			}
+		}
+	}
+	
+	//Invertir secuencias
+	public static void reverseSequense(int[][] matriz, int row, int start, int finish) {
+		
+		int changeValue;
+		
+		for(int i=start; i < finish; i++) {
+			
+			changeValue = matriz[row][i];
+			matriz[row][i] = matriz[row][finish];
+			matriz[row][finish] = changeValue;
+			finish--;
+		}
+			
+	}
+	
+	//Pedir valor al usuario
+	public static int askValue() {
+		
+		int value;
+		
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		
+		try{
+			System.out.println("¿Qué tamaño de secuencia desea reemplazar?");
+			value = new Integer(input.readLine());
+		}catch(Exception e) {
+			value = 0;
+		}
+		return value;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] matriz = new int[FIL][COL];
+		
+		loadingMatriz(matriz);
+		
+		printMatriz(matriz);
+		
+		searchSequence(matriz, askValue());
+		
+		printMatriz(matriz);
+	}
+
+}
